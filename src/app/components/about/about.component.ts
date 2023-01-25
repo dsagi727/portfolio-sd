0b6ac3b42db843c20f9576094669d9d65a627603
dsagi727 <dsagi727@gmail.com>
2023-01-25 21:55:17 +0100

    about done

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  selectLoad: string = 'skills';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public onSelect(selected: string): void {
+    this.selectLoad = selected;
   }
 
 }
