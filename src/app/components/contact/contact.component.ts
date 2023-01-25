3560444a420fd31e56d697be31b9b5591cb3ad3f
dsagi727 <dsagi727@gmail.com>
2023-01-25 21:55:55 +0100

    contact done

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { faLinkedin, faGithub  } from '@fortawesome/free-brands-svg-icons';
+import { faPaperPlane } from '@fortawesome/free-regular-svg-icons';
+
+import { faPhoneSquare } from '@fortawesome/free-solid-svg-icons';
+
+
+
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +15,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
+  faPaperPlane = faPaperPlane;
+  faPhoneSquare = faPhoneSquare;
+
+  contactForm!: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.contactForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      msg: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(255)]),
+    })
+  }
+
+  public sendLetter(): void {
+    const newLetter = this.contactForm.value;
+    if(newLetter){
+
+    }
   }
 
 }
