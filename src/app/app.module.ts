5280af21e367443a9c6c294ded2f8f31fd53a0b7
dsagi727 <dsagi727@gmail.com>
2023-01-25 15:53:55 +0100

    config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { StackComponent } from './components/stack/stack.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    FooterComponent,
+    StackComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
