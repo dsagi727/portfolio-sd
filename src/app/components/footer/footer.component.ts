2cfa4457f35bb479ea889318bc605f0c00e78b05
dsagi727 <dsagi727@gmail.com>
2023-01-25 21:56:06 +0100

    footer done

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faCopyright } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+
+  faCopyright = faCopyright;
+  actYear = new Date();
 
   constructor() { }
 
