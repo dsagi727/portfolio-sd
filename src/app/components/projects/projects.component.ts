95be2ec06c52970ace333190c4c8dc7ff7d9ee3a
dsagi727 <dsagi727@gmail.com>
2023-01-27 17:44:59 +0100

    done

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import { faExternalLinkAlt, faCode } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-projects',
@@ -9,6 +9,7 @@ import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 export class ProjectsComponent implements OnInit {
 
   faLink = faExternalLinkAlt;
+  faCode = faCode;
 
   constructor() { }
 
